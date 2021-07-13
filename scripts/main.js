0eb898ea4eb711fae2f09f85a09518ab8e2e766b
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-13 14:16:57 -0500

    userfavs displaying destination forcast including error alerting

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,34 @@
-document.querySelector(".submit").addEventListener("click", function() {
+document.querySelector(".submit").addEventListener("click", function () {
     getWeather(document.querySelector(".city").value)
 
 });
 
 function getWeather(cityName) {
     fetch('https://goweather.herokuapp.com/weather/' + cityName)
-        .then(function(response) {
+        .then(function (response) {
             return response.json()
         })
-        .then(function(data) {
+        .then(function (data) {
             const temp = data.temperature;
             const wind = data.wind;
             const description = data.description;
             buildWeather(temp, wind, description);
-        })
-}
+        }).catch((error) => {
 
+            return window.alert('Sorry, you city is not in our database');
+        });
+}
 function buildWeather(temp, wind, description) {
     //temp = temp * 1.8 + 32;
-    console.log(temp, wind, description);
-    const tempC = temp.replace(/[^0-9]/g, '');
-    const tempF = tempC * 1.8 + 32 + " F";
+    //console.log(temp, wind, description);
+    const tempC = temp.split(' ')[0];
+    const tempF = (tempC * 1.8 + 32).toFixed(1) + " F";
+    const city = document.querySelector(".city").value;
+    const firstName = document.querySelector(".userName").value;
 
-    console.log(tempF);
 
-    document.querySelector('.forcast').innerHTML = "this is our forecast";
+
+    document.querySelector('.forcast').innerHTML = `Hi ${firstName}! Currently, in ${city}, it is ${description.toLowerCase()} and ${tempF}. The wind speed is ${wind}.`;
 
 
 
