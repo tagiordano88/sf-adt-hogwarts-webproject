af55cfa6f05a8a5330a666cdd8fbbecd325f5369
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-15 10:54:44 -0500

    added picture rotation on index.html and tweaked display on tiffany.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-
 function displayNextImage() {
     x = (x === images.length - 1) ? 0 : x + 1;
     document.getElementById("img").src = images[x];
 }
-      
+
 function displayPreviousImage() {
     x = (x <= 0) ? images.length - 1 : x - 1;
     document.getElementById("img").src = images[x];
 }
-      
+
 function startTimer() {
     setInterval(displayNextImage, 3000);
 }
-      
-var images = [], x = -1;
-    images[0] = "../img/skyline.jpg";
-    images[1] = "../img/shoreline.jpg";
-    images[2] = "../img/coast.jpg";
-    images[3] = "../img/sandiego.jpg"; 
-            
+
+var images = [],
+    x = -1;
+images[0] = "../img/rana-sawalha-W_-6PWGbYaU-unsplash.jpg";
+images[1] = "../img/sunrise.jpg";
+images[2] = "../img/world.jpg";
+images[3] = "../img/journal.jpg";
+images[4] = "../img/sunset.jpg";
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-document.querySelector(".submit").addEventListener("click", function () {
+document.querySelector(".submit").addEventListener("click", function() {
     getWeather(document.querySelector(".city").value)
 
 });
 
 function getWeather(cityName) {
     fetch('https://goweather.herokuapp.com/weather/' + cityName)
-        .then(function (response) {
+        .then(function(response) {
             return response.json()
         })
-        .then(function (data) {
+        .then(function(data) {
             const temp = data.temperature;
             const wind = data.wind;
             const description = data.description;
@@ -18,6 +18,7 @@ function getWeather(cityName) {
             return window.alert('Sorry, you city is not in our database');
         });
 }
+
 function buildWeather(temp, wind, description) {
     //temp = temp * 1.8 + 32;
     //console.log(temp, wind, description);
